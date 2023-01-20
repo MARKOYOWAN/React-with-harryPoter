13f9d4fe680e932567dcade036a4d2466a1fd3fd
Marko <repository@idweb.space>
2023-01-20 17:51:33 +0300

    Install axios and call api

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,16 @@
-import React from 'react';
-  
-function HomeComponent (){
-    return <h1>Harry poter ok</h1>
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function HomeComponent() {
+  return (
+    <div className="dataHarry"> 
+    </div>
+  );
 }
-   
+
+const dataHarry = () => {
+  axios.get("https://hp-api.onrender.com/api/characters").then((res) => {
+    console.log("******Harry poter data************", res);
+  });
+};
+
 export default HomeComponent;
